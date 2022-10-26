9b5ab80d9e952a448c35b59efc16be9a8fadacb7
JoaquinFioriti <joaquinfioritifreelance@gmail.com>
2022-10-25 23:16:38 -0300

    se ve estp

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const server = http.createServer((req,res)=>{
         }else{
             errorHandler(400,"Endpoint no valido",res)
         }
-asdasdasdasdasd
+        
+        console.log("se ve esto?")
     }
 })
 
