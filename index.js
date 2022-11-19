c31e839878b9b59d9c2aec9988199c8440048813
JoaquinFioriti <joaquinfioritifreelance@gmail.com>
2022-11-19 20:39:47 -0300

    Limpieza de cutis

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ const server = http.createServer((req,res)=>{
         }else{
             errorHandler(400,"Endpoint no valido",res)
         }
-        
-        console.log("se ve esto?")
     }
 })
 
